9877eee9c15e445e75ea785daf830aeb1f5d2260
mutek <mutek@inventati.org>
2015-06-04 12:17:45 +0200

    [104591787] prepara-ricetta

--- UpdateGithub.sh
+++ UpdateGithub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
-# UpdateGithub.sh
+# UpdateGithub.sh v1.0.2
 #
 #
 # Copyleft )C( 2013-2015 Luca Cappelletti <luca.cappelletti@gmail.com>
@@ -25,20 +25,32 @@ git config --global user.name "mutek"
 git config --global user.email mutek@inventati.org
 
 commento=$1
+versione=$2
+
 [ -z $1 ] && commento=""
+
+# annota tags
 
 echo "Aggiungo i cambiamenti al database locale"
 git add -A .
-#echo "Inserisci una descrizione dei tuoi cambiamenti"
-#read n
+
 echo "Eseguo la finalizzazione dei cambiamenti in locale"
 EPOCA_UNIX=$(date +%N)
 COMMENTO="["$EPOCA_UNIX"] "$commento
+VERSIONE="v"$versione
+
 echo "Commento transazione: "$COMMENTO
 git commit -a -m "$COMMENTO"
+wait
 
 echo "Invio i cambiamenti locali al deposito remoto..."
 git push origin master
+wait
+
+[ -z $2 ] || { echo "Versione: "$VERSIONE; git tag -a $VERSIONE -m "$COMMENTO"; git push origin --tags; git tag; }
+wait
+
+[ -z $2 ] && git tag
+
 #git push origin gh-pages
 echo "ok"
-
